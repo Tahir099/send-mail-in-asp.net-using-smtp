20e7c060f2248876da2e4cdcfd51d49b550d288d
Tahir099 <sixlarovtahir13@gmail.com>
2022-12-10 14:49:52 +0400

    Add project files.

---
+++ project1/WebForm1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="project1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            &nbsp;Ətraflı<br />
+            <br />
+        </div>
+        <asp:Panel ID="Panel1" runat="server">
+            Kime&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox3" runat="server" Width="104px"></asp:TextBox>
+            <br />
+            <br />
+            Mövzu :&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList1" runat="server" Height="19px" Width="109px" AppendDataBoundItems="True" AutoPostBack="True" 
+            >
+            </asp:DropDownList>
+            <br />
+            <br />
+            Kontent:&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox4" runat="server" Width="104px" Height="20px" style="margin-top: 7px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ project1/WebForm1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Net.Mail;
+namespace project1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string Mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            getDropdown();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           // tahir = DropDownList1.SelectedItem.Text;
+       }
+      
+        protected void getDropdown()
+        {
+            SqlConnection sqlconn = new SqlConnection(Mainconn);
+            SqlCommand sqlcomm = new SqlCommand();
+            sqlcomm.Connection = sqlconn;
+            sqlcomm.CommandType = CommandType.StoredProcedure;
+            sqlcomm.CommandText = "select_topic";
+            sqlconn.Open();
+            DropDownList1.DataSource = sqlcomm.ExecuteReader();
+            DropDownList1.DataTextField = "topic_name";
+            DropDownList1.DataValueField = "topic_id";
+            DropDownList1.DataBind();
+            sqlconn.Close();
+            DropDownList1.Items.Insert(0, new ListItem("--Select Topic--"));
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MailMessage message = new MailMessage("sixlarovtahir13@gmail.com" , TextBox3.Text);
+            message.Subject = DropDownList1.SelectedItem.Text; //DropDownList1.SelectedItem.Text.ToString();
+            message.Body = TextBox4.Text;
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com" , 587);
+            smtpClient.Credentials = new System.Net.NetworkCredential()
+            {
+                UserName = "sixlarovtahir13@gmail.com",
+                Password = "iunbonstvvlpemur"
+            };
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(message);
+        }
+    }
+}
